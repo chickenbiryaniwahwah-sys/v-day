810f6ebb8a7b78cdaabaf8641d9ecd8bc8cb8e90
chickenbiryaniwahwah-sys <chickenbiryaniwahwah@gmail.com>
2026-04-18 12:19:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ const gifStages = [
 
 const noMessages = [
     "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
+    "whyyy??😔",
+    "please... 🥺",
+    "One round???",
+    "Bj atleast??😋",
     "Please??? 💔",
     "Don't do this to me...",
     "Last chance! 😭",
-    "You can't catch me anyway 😜"
+    "You can't catch me anyway 😈"
 ]
 
 const yesTeasePokes = [
